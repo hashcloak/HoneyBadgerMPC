a5196f8abfb09c4dd3e0a45a3db39bacc2e9f88f
Mikerah <mikerahqc@protonmail.com>
2020-08-24 11:24:04 -0400

    still buggy honeybadgerswap

--- apps/honeybadgerswap/honeybadgerswap.py
+++ apps/honeybadgerswap/honeybadgerswap.py
@@ -25,10 +25,10 @@ async def eth_to_token(
     init_token_pool: FixedPoint,
     eth_to_exchange: FixedPoint,
 ):
-    fee = eth_to_exchange / 500
-    k = init_eth_pool * init_token_pool
+    fee = await eth_to_exchange.div(500)
+    k = await (init_eth_pool * init_token_pool)
     new_eth_pool = init_eth_pool + eth_to_exchange
-    new_token_pool = k / (new_eth_pool - fee)
+    new_token_pool = await k.div(new_eth_pool - fee)
     tokens_to_receive = init_token_pool - new_token_pool
 
     return (tokens_to_receive, new_eth_pool, new_token_pool)
@@ -40,23 +40,27 @@ async def token_to_eth(
     init_token_pool: FixedPoint,
     tokens_to_exchange: FixedPoint,
 ):
-    fee = tokens_to_exchange / 500
-    k = init_eth_pool * init_token_pool
+    fee = await tokens_to_exchange.div(500)
+    k = await (init_eth_pool * init_token_pool)
     new_token_pool = init_token_pool + tokens_to_exchange
-    new_eth_pool = k / (new_token_pool - fee)
+    new_eth_pool = await k.div(new_token_pool - fee)
     eth_to_receive = init_eth_pool - new_eth_pool
 
     return (eth_to_receive, new_eth_pool, new_token_pool)
 
 
 def create_secret_share(ctx, x):
-    return FixedPoint(ctx, ctx.Share(x * 2 ** 32) + ctx.prepoc.get_zero(ctx))
+    return FixedPoint(ctx, ctx.Share(x * 2 ** 32) + ctx.preproc.get_zero(ctx))
+
+
+def create_clear_share(ctx, x):
+    return FixedPoint(ctx, ctx.Share(x * 2 ** 32))
 
 
 async def prog(ctx):
-    init_eth_pool = create_secret_share(ctx, 10)
-    init_token_pool = create_secret_share(ctx, 500)
-    eth_to_exchange = create_secret_share(ctx, 1)
+    init_eth_pool = create_clear_share(ctx, 10)
+    init_token_pool = create_clear_share(ctx, 500)
+    eth_to_exchange = create_clear_share(ctx, 1)
     tokens, new_eth_pool, new_token_pool = await eth_to_token(
         ctx, init_eth_pool, init_token_pool, eth_to_exchange
     )
